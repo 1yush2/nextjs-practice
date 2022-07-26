71cfa05d8ac06c5a5c0c3fabdc9dddd3abd6ef21
Aayush Karki <1yush225@gmail.com>
2022-07-27 00:07:32 +0545

    learned auto page routing links and mroe

---
+++ pages/chart-pages/chart.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function chart() {
+  return (
+    <div>
+      <Head>
+        <title>Chart Page</title>
+      </Head>
+      <h1>Hello I am Chart</h1>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.mainBody}>
-      <h1 className={styles.titleColor}>My Practice NextJS App</h1>
-    </div>
+    <>
+      <Head>
+        <title>Main App</title>
+      </Head>
+      <div className={styles.mainBody}>
+        <h1 className={styles.titleColor}>My Practice NextJS App</h1>
+        <Link href="chart-pages/chart">
+          <a>Go to this page Bro</a>
+        </Link>
+      </div>
+    </>
   );
 }
