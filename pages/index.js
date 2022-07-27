43f80246109ee9587a0edf8b04513706c1469d16
Aayush Karki <1yush225@gmail.com>
2022-07-27 13:18:40 +0545

    static site generation and server generation practice md

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Head from "next/head";
+import { getSortedChartsData } from "../utils/charts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allChartsData = getSortedChartsData();
+  return {
+    props: {
+      allChartsData,
+    },
+  };
+}
+
+
+export default function Home({ allChartsData }) {
   return (
     <>
       <Head>
@@ -14,6 +25,21 @@ export default function Home() {
           <a>Go to this page Bro</a>
         </Link>
       </div>
+
+      <section>
+        <h2>Blog</h2>
+        <ul>
+          {allChartsData.map(({ id, date, title }) => (
+            <li key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 }
